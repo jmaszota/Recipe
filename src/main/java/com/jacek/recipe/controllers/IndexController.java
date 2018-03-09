8e1d42b71ff5388baa2adc62fd03a5db9c626b07
Jacek Maszota <jacek.maszota@c-map.com>
2018-03-09 16:48:30 +0100

    First commit

---
+++ src/main/java/com/jacek/recipe/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.jacek.recipe.controllers;
+
+import  org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","/index"})
+    public String getIndexPage(){
+        System.out.println("asd");
+        return "Index";
+    }
+
+}
